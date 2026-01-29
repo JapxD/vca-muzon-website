db86d4e786591b2b5d171bf207ad9fbc97aef323
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-01-29 21:23:11 +0800

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -21,7 +21,7 @@ const NavBar = ({ logo, navBarItems }: NavBarProp) => {
   return (
     <nav
       className="relative w-full h-[98px] top-0 z-50 flex justify-between items-center
-            sticky shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] bg-white px-[50px] xl:px-[200px]"
+            sticky shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] bg-white px-[50px] xl:px-[150px]"
     >
       <Link to="/">
         <img src={logo} className="w-30 md:w-50" />
@@ -29,7 +29,7 @@ const NavBar = ({ logo, navBarItems }: NavBarProp) => {
 
       {/* Sidebard for mobile */}
       <div
-        className={`fixed md:hidden top-[98px] right-0 w-60 h-60 bg-white flex flex-col text-small items-start justify-evenly p-5 
+        className={`fixed md:hidden top-[98px] right-0 w-50 h-60 bg-white flex flex-col text-small items-start justify-evenly px-8 py-5 rounded-bl-xl 
             shadow-[-4px_4px_4px_0_rgba(0,0,0,0.25)] z-index-999 transition-all duration-300 
             ${barsOpen ? "translate-x-0 opacity-100 pointer-events-auto" : "translate-x-full opacity-0 pointer-events-none "}`}
       >
