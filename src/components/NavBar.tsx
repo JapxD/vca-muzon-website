5d23f254dc5d5f55312e340a316bb218fb1a0de1
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-01-29 15:01:14 +0800

    update navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,67 @@
-interface NavBarProp {
-  children: React.ReactNode;
-  padding?: number;
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import NavBarItem from "../components/NavBarItem";
+import { useState } from "react";
+
+interface NavBarItemType {
+  title: string;
+  to: string;
 }
 
-const NavBar = ({ children, padding }: NavBarProp) => {
+interface NavBarProp {
+  logo?: string;
+  navBarItems?: NavBarItemType[];
+}
+
+const NavBar = ({ logo, navBarItems }: NavBarProp) => {
+  const [barsOpen, setBarsOpen] = useState(false);
+
   return (
     <>
       <nav
-        className={`w-full h-[98px] top-0 z-50 flex justify-between items-center
-            sticky shadow-[0_0_4px_0_rgba(0,0,0,0.25)] bg-white px-${padding ? padding : 0}`}
+        className="relative w-full h-[98px] top-0 z-50 flex justify-between items-center
+            sticky shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] bg-white px-[50px]"
       >
-        {children}
+        <Link to="/">
+          <img src={logo} className="w-30 md:w-50" />
+        </Link>
+
+        {/* Sidebard for mobile */}
+        <div
+          className={`fixed md:hidden top-[98px] right-0 w-60 h-60 bg-white flex flex-col text-small items-start justify-evenly p-5 
+            shadow-[-4px_4px_4px_0_rgba(0,0,0,0.25)] z-index-999 transition-all duration-300 
+            ${barsOpen ? "translate-x-0 opacity-100 pointer-events-auto" : "translate-x-full opacity-0 pointer-events-none "}`}
+        >
+          {navBarItems &&
+            navBarItems.map((navBarItem) => (
+              <NavBarItem
+                key={navBarItem.title}
+                title={navBarItem.title}
+                to={navBarItem.to}
+              />
+            ))}
+        </div>
+
+        {/* Navbar Desktop */}
+        <div className="hidden md:flex gap-15 lg:gap-25 w-fit max-w-220 grow justify-end text-normal">
+          {navBarItems &&
+            navBarItems.map((navBarItem) => (
+              <NavBarItem
+                key={navBarItem.title}
+                title={navBarItem.title}
+                to={navBarItem.to}
+              />
+            ))}
+        </div>
+
+        {/* Navbard Mobile */}
+        <div className="w-fit flex md:hidden justify-end">
+          <button onClick={() => setBarsOpen(!barsOpen)}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
       </nav>
     </>
   );
--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 interface NavBarItemProp {
   title: string;
@@ -6,19 +6,18 @@ interface NavBarItemProp {
 }
 
 const NavBarItem = ({ title, to }: NavBarItemProp) => {
-  const location = useLocation(); // get current path
-  const isActive = location.pathname === to;
-
   return (
-    <Link
-      className={`relative text-neutral-800 font-semibold uppercase tracking-[0.04em] transition-scale duration-300 hover:-translate-y-1
+    <NavLink
+      className={({
+        isActive,
+      }) => `relative text-neutral-800 font-semibold uppercase tracking-[0.04em] transition-scale duration-300 hover:-translate-y-1
         after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:bg-neutral-800 after:transition-all after:duration-300
     ${isActive ? "after:w-full after:opacity-100" : "after:w-0 after:opacity-0"}
 `}
       to={to}
     >
       {title}
-    </Link>
+    </NavLink>
   );
 };
 
