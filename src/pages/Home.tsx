41a1d142da0242ccb6b8a1341be306a237315c48
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-01 17:30:48 +0800

    removed unused

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,7 @@
 import FullImageHeroSection from "../components/FullImageHeroSection";
 import blessedToBless from "../assets/blessed-to-bless.jpg";
-import blaze from "../assets/blaze.jpg";
 
 const Home = () => {
-  const paragraph =
-    "We’re a church family that loves God and loves people. Wherever you are in life, you’re welcome here. Come worship with us and find a place to belong.";
-  const cta = "Get Directions";
-
   return (
     <div className="w-full">
       <FullImageHeroSection image={blessedToBless} />
