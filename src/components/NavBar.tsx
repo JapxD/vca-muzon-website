6f117437895ed13a291e19d0c4cba0e94ffe1bee
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-01 17:25:03 +0800

    updated navbar to be transparent for full image layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,17 +19,14 @@ const NavBar = ({ logo, navBarItems }: NavBarProp) => {
   const [barsOpen, setBarsOpen] = useState(false);
 
   return (
-    <nav
-      className="relative w-full h-[98px] top-0 z-50 flex justify-between items-center
-            sticky shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] bg-white px-[50px] xl:px-[150px]"
-    >
+    <nav className="fixed w-full h-[98px] top-0 z-50 flex justify-between items-center shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] bg-transparent px-[50px] xl:px-[150px] text-white">
       <Link to="/">
         <img src={logo} className="w-30 md:w-50" />
       </Link>
 
       {/* Sidebard for mobile */}
       <div
-        className={`fixed md:hidden top-[98px] right-0 w-50 h-60 bg-white flex flex-col text-small items-start justify-evenly px-8 py-5 rounded-bl-xl 
+        className={`fixed md:hidden top-[98px] right-0 w-50 h-60 flex flex-col text-small items-start justify-evenly px-8 py-5 rounded-bl-xl 
             shadow-[-4px_4px_4px_0_rgba(0,0,0,0.25)] z-index-999 transition-all duration-300 
             ${barsOpen ? "translate-x-0 opacity-100 pointer-events-auto" : "translate-x-full opacity-0 pointer-events-none "}`}
       >
--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -10,8 +10,8 @@ const NavBarItem = ({ title, to }: NavBarItemProp) => {
     <NavLink
       className={({
         isActive,
-      }) => `relative text-neutral-800 font-semibold uppercase tracking-[0.04em] transition-scale duration-300 hover:-translate-y-1
-        after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:bg-neutral-800 after:transition-all after:duration-300
+      }) => `relative font-semibold uppercase tracking-[0.04em] transition-scale duration-300 hover:-translate-y-1
+        after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:bg-white after:transition-all after:duration-300
     ${isActive ? "after:w-full after:opacity-100" : "after:w-0 after:opacity-0"}
 `}
       to={to}
