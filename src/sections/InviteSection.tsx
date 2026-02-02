c52fd2d5fb098e92cff8e4c9c6c5a66b44df99f6
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-02 20:38:20 +0800

    update responsiveness

--- src/sections/InviteSection.tsx
+++ src/sections/InviteSection.tsx
@@ -3,24 +3,24 @@ import churchStage from "../assets/church-stage.jpg";
 
 const InviteSection = () => {
   return (
-    <section className="w-full h-fit md:h-110 lg:h-200 flex flex-col sm:flex-row gap-10 overflow-visible py-20 lg:px-15">
+    <section className="w-full h-fit md:h-110 xl:h-200 flex flex-col sm:flex-row gap-10 overflow-visible py-20 xl:px-15">
       <div className="w-full flex flex-col justify-center items-start px-10">
-        <h1 className="text-4xl lg:text-7xl bg-[var(--color-primary)] p-3 uppercase text-white rounded-lg">
+        <h1 className="text-4xl xl:text-7xl bg-[var(--color-primary)] p-3 uppercase text-white rounded-lg">
           Invite
         </h1>
-        <p className="py-2 lg:text-2xl">Your loved ones to church!</p>
-        <h2 className="text-4xl lg:text-6xl font-bold py-2 lg:py-4">
+        <p className="py-2 xl:text-2xl">Your loved ones to church!</p>
+        <h2 className="text-4xl xl:text-6xl font-bold py-2 xl:py-4">
           Sunday Service <br /> 9AM | 1PM (Youth) | 4PM
         </h2>
         <Link
-          className="text-[var(--color-primary)] py-2 lg:py-4 lg:text-2xl"
+          className="text-[var(--color-primary)] py-2 xl:py-4 xl:text-2xl"
           to="https://maps.app.goo.gl/nDP8xDbypsQCcG9w9"
           target="_blank"
         >
           2/F(Above Savemore), Muzon Central Terminal, <br />
           540 M. Villarica Rd, SJDM, 3023 Bulacan
         </Link>
-        <p className="lg:text-2xl lg:py-4">
+        <p className="xl:text-2xl xl:py-4">
           Join us at VCA Muzon and experience worship, <br />
           community, and God’s love every Sunday.
         </p>
