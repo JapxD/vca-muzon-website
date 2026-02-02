dd3dd07325feda2df19fcf0e6b9fc9343e0fcee5
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-02 17:00:34 +0800

    Creating new section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import FullImageHeroSection from "../components/FullImageHeroSection";
+import FullImageHeroSection from "../sections/FullImageHeroSection";
 import blessedToBless from "../assets/blessed-to-bless.jpg";
+import InviteSection from "../sections/InviteSection";
 
 const Home = () => {
   return (
     <div className="w-full">
       <FullImageHeroSection image={blessedToBless} />
+      <InviteSection />
     </div>
   );
 };
---
+++ src/sections/InviteSection.tsx
@@ -0,0 +1,9 @@
+const InviteSection = () => {
+  return (
+    <section className="relative h-screen flex py-20 justify-center text-[var(--color-text-primary)] gap-10">
+      <div className=""></div>
+    </section>
+  );
+};
+
+export default InviteSection;
