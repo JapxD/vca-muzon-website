a9784c240953c963ffab9c0719f63faba32c9180
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-03 23:03:59 +0800

    configuration for hot reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // important for Docker port: 5173,
+    watch: {
+      usePolling: true, // forces file change detection inside Docker
+    },
+  },
 });
