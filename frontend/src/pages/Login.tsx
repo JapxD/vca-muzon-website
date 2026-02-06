670c1ad7605832e4669346cebfb811c5af1d2c60
Jasper Garcia <garciajaspermercado24@gmail.com>
2026-02-06 21:13:03 +0800

    improved login to have background image

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import blessedToBless from "../assets/blessed-to-bless.jpg";
 
 const Login = ({}) => {
   const [email, setEmail] = useState("");
@@ -10,9 +11,16 @@ const Login = ({}) => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <img
+        src={blessedToBless}
+        className="absolute inset-0 w-full h-full object-cover"
+        alt="Church worship"
+      />
+
+      <div className="absolute inset-0 bg-[var(--color-primary-dark)]/80"></div>
       <form
         onSubmit={handleSubmit}
-        className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md"
+        className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md z-999"
       >
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
